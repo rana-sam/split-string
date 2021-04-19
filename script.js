0f1e3e213d9a09dd9d281cdeff657c559732d298
Muhammad Usama <82697108+rana-sam@users.noreply.github.com>
2021-04-19 22:27:58 +0500

    split and join

---
+++ script.js
@@ -0,0 +1,9 @@
+let names = "salik usama ahmad ali usman abubaakr awais";
+
+console.log("Before: "+names);
+let nameArray = names.split(' ');//convert string into array
+
+names = nameArray.join('ali');//convert array into string
+console.log(names);
+
+
